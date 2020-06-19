143506b20bc968e69a2a4de9bcee9a117ee59e51
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-06-19 16:07:50 +0200

    Update game.js

--- javascript/game.js
+++ javascript/game.js
@@ -1,14 +1,16 @@
 var rows = 10;
 var cols = 10;
-var squareSize = 50;
+var fieldSize = 50;
 
 var game = document.getElementById('game');
 
 //grid
 for(i=0; i<cols; i++) {
     for(j=0; j<rows; j++) {
-        var square = document.createElement('div');
-        game.appendChild(square);
-        square.id = j.concat(i);
+        var field = document.createElement('div');
+        game.appendChild(field);
+        field.id = j.concat(i);
+        field.style.top = j*fieldSize + 'px';
+        field.style.left = i*fieldSize + 'px';
     }
 }
